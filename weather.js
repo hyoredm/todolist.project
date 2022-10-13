4d298bc20bf80a98aa0b63abbb3e3a7c771d69f9
hyoredm <hyoredm@gmail.com>
2022-10-13 22:57:09 +0900

    commit

--- weather.js
+++ weather.js
@@ -2,15 +2,15 @@ const API_KEY = "dc214fa675b5e7af10018a6407916a38"
 
 function onGeo0k(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
     fetch(url).then(response => response.json())
     .then(data => {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:first-child");
-        city.innerText = data.name;
-        weather.innerText = `data.weather[0].main / ${data.main,temp}`;
+        city.innerText = `${data.name}`;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 function onGeoError() {
